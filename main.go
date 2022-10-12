904396e826be4bc55dfe3e2338f3e4104811c45e
Alejandro Yepes <alejandroyepes.ing@gmail.com>
2022-10-11 21:05:01 -0500

    improve: import handlers

--- main.go
+++ main.go
@@ -3,29 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	h "github.com/Alek-ing/API-http-go/handlers"
 )
 
 func main() {
-	http.HandleFunc("/es", handlerSpanish)
+	http.HandleFunc("/es", h.HandlerSpanish)
 
-	http.HandleFunc("/en", handlerEnglish)
+	http.HandleFunc("/en", h.HandlerEnglish)
 
-	http.HandleFunc("/pt", handlerPortuguese)
+	http.HandleFunc("/pt", h.HandlerPortuguese)
 
 	address := ":8080"
 	fmt.Println("server running ", address)
 	http.ListenAndServe(address, nil)
 
-}
-
-func handlerSpanish(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(resp, "hola")
-}
-
-func handlerEnglish(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(resp, "hello")
-}
-
-func handlerPortuguese(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(resp, "ola")
 }
